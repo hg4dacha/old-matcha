73b3bdd2d381b49cd10cb866e12039e81b5d802b
hg4dacha <hgadacha@student.42.fr>
2021-04-06 18:21:39 +0200

    matcha

---
+++ src/components/Home/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <div className='bg-light'>
+                <a class="btn btn-primary" href="#" role="button">Se connecter</a>
+            </div>
+        </header>
+    );
+}
+
+export default Header
---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './Header'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <Header />
+    );
+}
+
+export default Home

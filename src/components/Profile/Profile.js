04561bffbc3345fec3fee91d04c99a9f8f3220a6
hg4dacha <hgadacha@student.42.fr>
2021-04-28 13:04:15 +0200

    matcha

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import Navbar from '../NavBar/NavBar';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl'
 import { RiUser3Fill } from 'react-icons/ri';
 
 const Profile = () => {
@@ -16,6 +18,18 @@ const Profile = () => {
                     <h1 className='FormsTittle center'>
                         <RiUser3Fill size='22' className='iconsFormsTittles' />
                     Profil</h1>
+                </div>
+                <div>
+                <InputGroup className="mb-3">
+                    <InputGroup.Prepend>
+                        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                    </InputGroup.Prepend>
+                    <FormControl
+                        placeholder="Username"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
                 </div>
             </div>
         </div>

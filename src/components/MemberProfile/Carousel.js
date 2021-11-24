d87a363a3d93939a313af97a60772d1c3a7b4407
hgadacha <hgadacha@student.42.fr>
2021-11-24 19:56:42 +0100

    matcha

--- src/components/MemberProfile/Carousel.js
+++ src/components/MemberProfile/Carousel.js
@@ -1,29 +1,47 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import { v4 as uuidv4 } from 'uuid';
-
+import React, { Fragment, useState } from 'react';
 
 
 
 
 const Carousel$ = ({UserPhotos}) => {
 
-    const controls = UserPhotos.length === 1 ? false : true ;
 
-    const nodeRef = React.useRef(null);
+    const [photo, setPhoto] = useState(0)
 
+    const previousImg = () => {
+        photo > 0 && photo <= (UserPhotos.length - 1) ?
+        setPhoto(photo - 1) :
+        setPhoto(UserPhotos.length - 1) ;
+    }
+
+    const nextImg = () => {
+        photo >= 0 && photo < (UserPhotos.length - 1) ?
+        setPhoto(photo + 1) :
+        setPhoto(0) ;
+    }
+
+    const buttons = UserPhotos.length === 1 ?
+                    null                    :
+                    <Fragment>
+                        <button onClick={previousImg} className="carousel-control-prev btn-prev-nxt" type="button" data-bs-target="#carouselUserImage" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        </button>
+                        <button onClick={nextImg} className="carousel-control-next btn-prev-nxt" type="button" data-bs-target="#carouselUserImage" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        </button>
+                    </Fragment>             ;
+    
+
+    
     return (
-        <Carousel nodeRef={nodeRef} fade={true} interval={null} indicators={false} controls={controls} className='carousel-div'>
-
-            {UserPhotos.map( data => {
-                return (
-                    <Carousel.Item ref={nodeRef} key={uuidv4()}>
-                        <img className="d-block w-100" src={data} alt="Utilisateur" />
-                    </Carousel.Item>
-                )
-            })}
-
-        </Carousel>
+        <div id="carouselUserImage" className="carousel slide carousel-div" data-bs-ride="carousel" data-interval="2000">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src={UserPhotos[photo]} className="d-block w-100 img-fluid" alt="user"/>
+                </div>
+            </div>
+            {buttons}
+        </div>
     )
 }
 
--- src/components/MemberProfile/MemberProfile.js
+++ src/components/MemberProfile/MemberProfile.js
@@ -1,19 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Navbar from '../NavBar/NavBar';
 import Chat from './Chat';
 import Carousel from './Carousel';
 import Button from 'react-bootstrap/Button'
 import { MdBlock } from "react-icons/md";
 import { GoPrimitiveDot } from "react-icons/go";
+import { IoMdHeartEmpty, IoMdHeart } from "react-icons/io";
+import { AiFillStar } from 'react-icons/ai';
 
 import jeanma1 from '../../images/jeanma1.jpg'
 import jeanma2 from '../../images/jeanma2.jpg'
 import jeanma3 from '../../images/jeanma3.jpg'
+import selfie from '../../images/selfie.jpg'
 
 
 const MemberProfile = () => {
 
-    const UserPhotos = [jeanma1, jeanma2, jeanma3]
+    const UserPhotos = [jeanma1, jeanma2, jeanma3, selfie]
+
+    // LIKE DISLIKE ↓↓↓
+    const [like, setLike] = useState(false)
+
+    const likeDislike = () => {
+        setLike(!like)
+    }
+
+    const heart = like ?
+                  <Button variant="outline-danger" onClick={likeDislike} style={{boxShadow: '0px 0px 10px 3px white', borderColor: '#dc3545', backgroundColor: '#dc3545'}}>
+                      <IoMdHeart className='like-heart' color='darkred' />
+                  </Button> :
+                  <Button variant="outline-danger" onClick={likeDislike} style={{boxShadow: 'none', borderColor: '#725551'}}>
+                      <IoMdHeartEmpty className='like-heart' color='#725551
+' />
+                  </Button> ;
 
     return (
         <Fragment>
@@ -27,14 +46,19 @@ const MemberProfile = () => {
                 </div>
                 <div className='infos-part'>
                     <div className='infos-list'>
-                        <span>username-269428</span>
-                        <div><GoPrimitiveDot/><span>En ligne</span></div>
+                        <div>
+                            <span>username-269428</span>
+                            <span className='popularity'><AiFillStar className='star'/>1425°</span>
+                            {heart}
+                        </div>
+                        <div className='centerElementsInPage'><GoPrimitiveDot color='#009432' /><span>En ligne</span></div>
+                        <div className='centerElementsInPage'><GoPrimitiveDot color='#353b48' /><span>Hors ligne</span></div>
                         <span>Nom</span>
                         <span>Prenom</span>
                         <span>Age</span>
                         <span>Sexe</span>
                         <span>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l\'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n\'a pas fait que survivre cinq siècles, mais s\'est aussi adapté à la bureautique informatique, sans que son contenu n\'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</span>
-                        <Button variant="danger"><MdBlock/>Bloquer</Button>
+                        <Button variant="danger" className='centerElementsInPage' style={{width: 'fit-content'}} ><MdBlock/>Bloquer</Button>
                     </div>
                 </div>
             </div>
